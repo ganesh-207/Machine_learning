79e2efbbffedd4772bc4710995337b16071dee3d
ganesh-207 <salunkheganesh5948@gmail.com>
2025-11-14 17:22:09 +0530

    machine project

--- Project/Classification Project/Data_clean_preprocess.py
+++ Project/Classification Project/Data_clean_preprocess.py
@@ -2,13 +2,13 @@
 from sklearn.pipeline import make_pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import StandardScaler,OneHotEncoder
+from sklearn.preprocessing import StandardScaler,OrdinalEncoder
 
 def clean_process_data(cat,con,X):
     # Creating numerical pipeline that handles missing data and also scales numerical data
     con_pipe = make_pipeline(SimpleImputer(strategy='mean'),StandardScaler())
     # Creating categorical pipeline that handles missing data and scales text data
-    cat_pipe = make_pipeline(SimpleImputer(strategy='most_frequent'),OneHotEncoder(handle_unknown='ignore',sparse_output=False))
+    cat_pipe = make_pipeline(SimpleImputer(strategy='most_frequent'),OrdinalEncoder())
     # Combine both the piplines
     pre = ColumnTransformer([
         ('cat',cat_pipe,cat),
